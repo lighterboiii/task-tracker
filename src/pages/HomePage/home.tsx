7886966f24551c86e4ed9d4a8b2b38216be3b1d8
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2023-10-25 16:45:34 +0300

    fix: исправления в позиционировании элементов

--- src/pages/HomePage/home.tsx
+++ src/pages/HomePage/home.tsx
@@ -4,18 +4,14 @@ import styles from './home.module.scss';
 
 const HomePage: FC = () => {
   return (
-    <div className={styles.home}>
-      <div className={styles.home__element}>
-        <Link className={styles.home__button} to="/boards">
-          Your boards
-        </Link>
-      </div>
-      <div className={styles.home__element}>
-        <button type="button" className={styles.home__button}>
-          Create board
-        </button>
-      </div>
-    </div>
+    <nav className={styles.home}>
+      <Link className={styles.home__element} to="/boards">
+        Your boards
+      </Link>
+      <Link className={styles.home__element} to="/create">
+        Create board
+      </Link>
+    </nav>
   );
 };
 
